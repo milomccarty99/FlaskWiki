549920f4d4e7ed1a342c7264d5c0f75bf6a445a5
Milo McCarty <milomccarty99@gmail.com>
2023-03-02 05:27:26 -0800

    basic functionality

--- Website/app.py
+++ Website/app.py
@@ -4,28 +4,78 @@ from flask_bootstrap import Bootstrap5
 from pymongo import MongoClient
 import requests
 import datetime
+import markdown
 import sys
 import os.path
 import json
 import re
 import secrets
+import random 
+from werkzeug.utils import secure_filename
 
 import constants
 
 app = flask.Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'static/images'
 bootsrtap = Bootstrap5(app)
 
-mongoclient = MongoClient("mongodb://127.0.0.1:27017")
-
+mongoclient = MongoClient("mongodb://192.168.92.21:27017")
+pagecollections = mongoclient["pagecollections"]
+wikiarticles = pagecollections["wikiarticles"]
 
 @app.route('/',methods=['GET','POST'])
 def home_page():
-    return "Hello World"
+    return render_template('homepage.html')
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_page():
+    if request.method == 'POST':
+        article_id = request.form.get("id")
+        article_title = request.form.get("title")
+        article_body = request.form.get("articleText")
+        
+        wikiarticles.insert_one({"_id": article_id, "title":article_title,"md":article_body})
+        return redirect('/page/' + article_id)
+    return render_template('add.html')
 
 @app.route('/page/<id>', methods=['GET','POST'])
 def site_page(id):
-    return "Hello World " + str(id)
+    page_info = wikiarticles.find_one({"_id": id})
+    if not page_info:
+        return redirect(url_for('page_not_found'))
+    title = page_info.get("title")
+    mdtext = page_info.get("md")
+    pagebody= markdown.markdown(mdtext)
+    return render_template('page.html',id=id,title=title,pagebody=pagebody)
 
+@app.route('/page/edit/<id>', methods=['GET','POST'])
+def edit_site_page(id):
+    return "editing " + str(id)
+
+@app.route('/articles', methods=['GET', 'POST'])
+def articles_page():
+    allarticles = list(wikiarticles.find())
+    numarticles = len(allarticles)
+    if numarticles <= 0:
+        return "no pages are set up"
+    return render_template('pagelist.html',articles=allarticles)
+
+@app.route('/random', methods=['GET', 'POST'])
+def random_page():
+    allarticles = list(wikiarticles.find())
+    numarticles = len(allarticles)
+    if numarticles <= 0:
+        return "no pages are set up"
+    articleselected = random.randrange(numarticles)
+    return redirect('/page/' + allarticles[articleselected].get("_id"))
+
+@app.route('/pagenotfound')
+def page_not_found():
+    return "lol"
+
+@app.route('/users', methods=['GET','POST'])
+def users_page():
+    return "list users here"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port = 80)
